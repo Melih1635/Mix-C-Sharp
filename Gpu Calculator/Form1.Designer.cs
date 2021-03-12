5dedfe20512adf86bf5ac2116d2e42b0d6765729
Melih1635 <63011591+Melih1635@users.noreply.github.com>
2021-03-12 22:08:09 +0300

    Add files via upload

---
+++ Gpu Calculator/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace _2.Dönem_2.Ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArkadas = new System.Windows.Forms.Button();
+            this.btnSon = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.lblListX = new System.Windows.Forms.Label();
+            this.lblListY = new System.Windows.Forms.Label();
+            this.lblToplam = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnArkadas
+            // 
+            this.btnArkadas.BackColor = System.Drawing.SystemColors.Control;
+            this.btnArkadas.Location = new System.Drawing.Point(25, 244);
+            this.btnArkadas.Name = "btnArkadas";
+            this.btnArkadas.Size = new System.Drawing.Size(113, 33);
+            this.btnArkadas.TabIndex = 0;
+            this.btnArkadas.Text = "ARKADAŞ MI";
+            this.btnArkadas.UseVisualStyleBackColor = false;
+            this.btnArkadas.Click += new System.EventHandler(this.btnArkadas_Click);
+            // 
+            // btnSon
+            // 
+            this.btnSon.BackColor = System.Drawing.SystemColors.Control;
+            this.btnSon.Location = new System.Drawing.Point(158, 244);
+            this.btnSon.Name = "btnSon";
+            this.btnSon.Size = new System.Drawing.Size(75, 33);
+            this.btnSon.TabIndex = 1;
+            this.btnSon.Text = "SON";
+            this.btnSon.UseVisualStyleBackColor = false;
+            this.btnSon.Click += new System.EventHandler(this.btnSon_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(40, 71);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(17, 17);
+            this.lblX.TabIndex = 2;
+            this.lblX.Text = "X";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(40, 132);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(17, 17);
+            this.lblY.TabIndex = 3;
+            this.lblY.Text = "Y";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(123, 71);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 4;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(123, 132);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.TabIndex = 5;
+            // 
+            // lblListX
+            // 
+            this.lblListX.AutoSize = true;
+            this.lblListX.Location = new System.Drawing.Point(453, 55);
+            this.lblListX.Name = "lblListX";
+            this.lblListX.Size = new System.Drawing.Size(0, 17);
+            this.lblListX.TabIndex = 6;
+            // 
+            // lblListY
+            // 
+            this.lblListY.AutoSize = true;
+            this.lblListY.Location = new System.Drawing.Point(725, 55);
+            this.lblListY.Name = "lblListY";
+            this.lblListY.Size = new System.Drawing.Size(0, 17);
+            this.lblListY.TabIndex = 7;
+            // 
+            // lblToplam
+            // 
+            this.lblToplam.AutoSize = true;
+            this.lblToplam.Location = new System.Drawing.Point(263, 315);
+            this.lblToplam.Name = "lblToplam";
+            this.lblToplam.Size = new System.Drawing.Size(0, 17);
+            this.lblToplam.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(983, 450);
+            this.Controls.Add(this.lblToplam);
+            this.Controls.Add(this.lblListY);
+            this.Controls.Add(this.lblListX);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.btnSon);
+            this.Controls.Add(this.btnArkadas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArkadas;
+        private System.Windows.Forms.Button btnSon;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label lblListX;
+        private System.Windows.Forms.Label lblListY;
+        private System.Windows.Forms.Label lblToplam;
+    }
+}
+
---
+++ Gpu Calculator/Form1.cs
@@ -0,0 +1,105 @@
+﻿/***********************************************************************************
+**                                    SAKARYA ÜNİVERSİTESİ
+**                          BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**                             BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**                          NESNEYE DAYALI PROGRAMLAMA DERSİ
+**                            2019-2020 BAHAR DÖNEMİ
+**                          
+**                         
+**                            ÖDEV NUMARASI.............: 2.ÖDEV
+**                          ÖĞRENCİ ADI SOYADI........:   MELİH ÖZMEN
+**                          ÖĞRENCİ NUMARASI..........:   B191210079
+**                           DERSİN ALINDIĞI GRUP......:   B GRUBU
+**************************************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.Dönem_2.Ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArkadas_Click(object sender, EventArgs e)
+        {   
+            //Burada butona basıldıktan sonra oluşturalacak textbox ve listbox kodları yer alıyor.
+            var textBox1 = new TextBox
+            {
+                Left = 300,
+                Top = 250,
+            };
+            var textBox2 = new TextBox
+            {
+                Left = 500,
+                Top = 250,
+            };
+            var listBox1 = new ListBox
+            {
+                Left = 300,
+                Top = 100,
+            };
+            var listBox2 = new ListBox
+            {
+                Left = 500,
+                Top = 100,
+            };
+            //textbox ve listbox eklemek için yazılır
+            Controls.Add(textBox1); Controls.Add(textBox2); Controls.Add(listBox1); Controls.Add(listBox2);
+            
+            lblListX.Text = "X"; lblListY.Text = "Y"; lblToplam.Text = "TOPLAMLAR";
+            int sayiX, sayiY;
+            //Textboxdaki metni sayıya çevirmek için 
+            sayiX = Convert.ToInt32(txtX.Text);
+            sayiY = Convert.ToInt32(txtY.Text);
+            int XbolenToplamı = 0; int YbolenToplamı = 0;
+            //X sayısının bölenlerinin hesaplanması ve toplam bölen değerinin bulunması için yapılan döngü
+            for (int m = 1; m <= sayiX; m++)
+              {
+                  if (sayiX % m == 0)
+                     {
+                        listBox1.Items.Add(m.ToString());
+                         XbolenToplamı += m;
+                     }                  
+              }
+             
+            //Y sayısının bölenlerinin hesaplanması ve toplam bölen değerinin bulunması için yapılan döngü
+            for (int k = 1; k <= sayiY; k++)
+              {
+                  if (sayiY % k == 0)
+                  {
+
+                      listBox2.Items.Add(k.ToString());
+                      YbolenToplamı += k;
+                  }
+              }
+             
+             textBox1.Text = XbolenToplamı.ToString();//X bölenlerinin toplamını yazdırır
+              textBox2.Text = YbolenToplamı.ToString();//Y bölenlerinin toplamını yazdırır
+
+        }
+
+        private void btnSon_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+
+      
+        
+    
+
+   
+
